0bed0545afa013b2555047b08d8f8804785e723a
AsafDavidov <adavidov@conncoll.edu>
2018-12-28 12:27:33 -0500

    improve login styling

--- fantasy-finance-frontend/src/components/NewsStrip.js
+++ fantasy-finance-frontend/src/components/NewsStrip.js
@@ -4,8 +4,8 @@ import {Divider} from "semantic-ui-react"
 const NewStrip = ({news})=>{
    return(
      <div>
+      <h1 style={{marginTop:"10px",fontSize:"40px",textAlign:"left"}}> Recent News <span style={{fontSize:"20px"}}>provided by the New York Times</span></h1>
       <Divider />
-      <h1 style={{fontSize:"40px",textAlign:"left"}}> Recent News <span style={{fontSize:"20px"}}>provided by the New York Times</span></h1>
       {news.map(article=><NewsBlock key={article.title} title={article.title} abstract={article.abstract} img={article.imgSrc} url={article.url}/>)}
      </div>
    )
--- fantasy-finance-frontend/src/components/login.js
+++ fantasy-finance-frontend/src/components/login.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { Message, Form, Input, Button } from 'semantic-ui-react'
+import { Header,
+         Message,
+         Form,
+         Input,
+         Button,
+         Grid,
+         Segment,
+         Divider } from 'semantic-ui-react'
 import {connect} from "react-redux";
 import * as actions from '../store/actions/user';
 import "../Profile.css"
@@ -32,44 +39,52 @@ import "../Profile.css"
       if (this.state.password === this.state.passwordConfirmation){
         return null
       }else{
-        return <Message warning style={{width:"70%"}} header={"Passwords must equal each other"}/>
+        return <Message warning style={{width:"60%",marginLeft:"20%"}} header={"Passwords must equal each other"}/>
       }
     }
   }
   render(){
     return(
-      <div style={{marginLeft:"2%",marginTop:"2%",width:"40%"}}>
-        <h1>Login</h1>
-        {this.props.failedLogin ? <Message error style={{width:"70%"}} header={this.props.message}/> : null}
-        <Form size={"huge"} onSubmit={this.handleLoginUser}>
-        <Form.Field style={{width:"70%"}}required>
-          <label>Username</label>
-          <Input name={"loginUsername"} placeholder='Username' value={this.state.loginUsername}onChange={this.handleChange}/>
-          <label>Password</label>
-          <Input type="password" name={"loginPassword"} placeholder='Password' value={this.state.loginPassword}onChange={this.handleChange}/>
-        </Form.Field >
-          <Button type='submit'>Submit</Button>
-        </Form>
-        <br></br>
-        <h1>Sign-Up</h1>
-        {this.checkPasswords()}
-        {this.props.failedSignup ? <Message error style={{width:"70%"}} header={this.props.message}/> : null}
-        <Form size={"huge"} onSubmit={this.handleNewUser}>
-        <Form.Field style={{width:"70%"}}>
-          <label>First Name</label>
-          <Input name={"first_name"} placeholder='First Name' value={this.state.first_name}onChange={this.handleChange}/>
-          <label>Last Name</label>
-          <Input name={"last_name"} placeholder='Last Name' value={this.state.last_name}onChange={this.handleChange}/>
-          <label>Username</label>
-          <Input name={"username"} placeholder='Username must be unique!' value={this.state.username}onChange={this.handleChange}/>
-          <label>Password</label>
-          <Input type="password" name={"password"} placeholder='Password' value={this.state.password}onChange={this.handleChange}/>
-          <label>Password Confirmation</label>
-          <Input type="password" name={"passwordConfirmation"} placeholder='Password Confirmation' value={this.state.passwordConfirmation}onChange={this.handleChange}/>
-
-        </Form.Field>
-        <Button type='submit'>Sign Up</Button>
-        </Form>
+      <div style={{marginLeft:"10%",marginTop:"5%",width:"80%"}}>
+        <h1 style={{textAlign:'center'}}>Welcome to FantasyTrader</h1>
+        <Segment placeholder>
+          <Grid columns={2} relaxed='very' stackable>
+            <Grid.Column>
+              <Header style={{textAlign:"center"}} as="h1">Login</Header>
+              {this.props.failedLogin ? <Message error style={{width:"60%",marginLeft:"20%"}} header={this.props.message}/> : null}
+              <Form size={"huge"} onSubmit={this.handleLoginUser}>
+              <Form.Field style={{minWidth:"60%"}}>
+                <label style={{color:"blue"}}>Username</label>
+                <Input name={"loginUsername"} placeholder='Username' value={this.state.loginUsername}onChange={this.handleChange}/>
+                <label style={{color:"blue"}}>Password</label>
+                <Input type="password" name={"loginPassword"} placeholder='Password' value={this.state.loginPassword}onChange={this.handleChange}/>
+              </Form.Field >
+                <Button type='submit'>Submit</Button>
+              </Form>
+            </Grid.Column>
+            <Grid.Column>
+              <Header style={{textAlign:"center"}} as="h1">Sign-Up</Header>
+              {this.checkPasswords()}
+              {this.props.failedSignup ? <Message error style={{width:"60%",marginLeft:"20%"}} header={this.props.message}/> : null}
+              <Form size={"huge"} onSubmit={this.handleNewUser}>
+              <Form.Field style={{minWidth:"60%"}}>
+                <label style={{color:"blue"}}>First Name</label>
+                <Input name={"first_name"} placeholder='First Name' value={this.state.first_name}onChange={this.handleChange}/>
+                <label style={{color:"blue"}}>Last Name</label>
+                <Input name={"last_name"} placeholder='Last Name' value={this.state.last_name}onChange={this.handleChange}/>
+                <label style={{color:"blue"}}>Username</label>
+                <Input name={"username"} placeholder='Username must be unique!' value={this.state.username}onChange={this.handleChange}/>
+                <label style={{color:"blue"}}>Password</label>
+                <Input type="password" name={"password"} placeholder='Password' value={this.state.password}onChange={this.handleChange}/>
+                <label style={{color:"blue"}}>Password Confirmation</label>
+                <Input type="password" name={"passwordConfirmation"} placeholder='Password Confirmation' value={this.state.passwordConfirmation}onChange={this.handleChange}/>
+              </Form.Field>
+              <Button type='submit'>Sign Up</Button>
+              </Form>
+            </Grid.Column>
+          </Grid>
+          <Divider vertical>Or</Divider>
+        </Segment>
       </div>
     )
   }
