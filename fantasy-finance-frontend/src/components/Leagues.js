7e4e9654110d6daa30959b4cb159c6ca06f501c5
AsafDavidov <adavidov@conncoll.edu>
2019-01-03 09:45:30 -0500

    separate active and inactive leagues

--- fantasy-finance-frontend/src/components/Leagues.js
+++ fantasy-finance-frontend/src/components/Leagues.js
@@ -3,8 +3,16 @@ import League from './League'
 import {Card} from "semantic-ui-react"
 const Leagues = (props) => {
   return (
-    <div style={{marginTop:"10px"}}>
-      {props.leagues.length>0 ? <Card.Group centered itemsPerRow={5}>{props.leagues.map(league=>(<Card key={league.id} color='blue'><League balance={league.start_balance} name={league.name} id={league.id}/></Card>))}</Card.Group>:<h1>Go Join a League!</h1>}
+    <div>
+      <div style={{marginTop:"10px"}}>
+        <h1>Active Leagues</h1>
+
+        {props.leagues.length>0 ? <Card.Group centered itemsPerRow={5}>{props.leagues.filter(league=>new Date(league.end_date)<=new Date).map(league=>(<Card key={league.id} color='blue'><League endDate={league.end_date}balance={league.start_balance} name={league.name} id={league.id}/></Card>))}</Card.Group>:<h1>Go Join a League!</h1>}
+      </div>
+      <div style={{marginTop:"20px"}}>
+        <h1>Inactive Leagues</h1>
+        {props.leagues.length>0 ? <Card.Group centered itemsPerRow={5}>{props.leagues.filter(league=>new Date(league.end_date)>new Date).map(league=>(<Card key={league.id} color='blue'><League endDate={league.end_date}balance={league.start_balance} name={league.name} id={league.id}/></Card>))}</Card.Group>:<h1>Go Join a League!</h1>}
+      </div>
     </div>
   )
 };
