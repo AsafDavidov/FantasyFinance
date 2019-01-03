a087c910303afe2755d4230c818dc1c82181543d
AsafDavidov <adavidov@conncoll.edu>
2019-01-03 11:42:53 -0500

    fix active/inactive leagues

--- fantasy-finance-frontend/src/components/Leagues.js
+++ fantasy-finance-frontend/src/components/Leagues.js
@@ -6,11 +6,11 @@ const Leagues = (props) => {
     <div>
       <div style={{marginTop:"10px"}}>
         <h1>Active Leagues</h1>
-        {props.leagues.length>0 ? <Card.Group centered itemsPerRow={5}>{props.leagues.filter(league=>new Date(league.end_date)<=new Date).map(league=>(<Card key={league.id} color='blue'><League endDate={league.end_date}balance={league.start_balance} name={league.name} id={league.id}/></Card>))}</Card.Group>:<h1>Go Join a League!</h1>}
+        {props.leagues.length>0 ? <Card.Group centered itemsPerRow={5}>{props.leagues.filter(league=>new Date(league.end_date)>=new Date).map(league=>(<Card key={league.id} color='blue'><League endDate={league.end_date}balance={league.start_balance} name={league.name} id={league.id}/></Card>))}</Card.Group>:<h1>Go Join a League!</h1>}
       </div>
       <div style={{marginTop:"20px"}}>
         <h1>Inactive Leagues</h1>
-        {props.leagues.length>0 ? <Card.Group centered itemsPerRow={5}>{props.leagues.filter(league=>new Date(league.end_date)>new Date).map(league=>(<Card key={league.id} color='blue'><League endDate={league.end_date}balance={league.start_balance} name={league.name} id={league.id}/></Card>))}</Card.Group>:<h1>Go Join a League!</h1>}
+        {props.leagues.length>0 ? <Card.Group centered itemsPerRow={5}>{props.leagues.filter(league=>new Date(league.end_date)<new Date).map(league=>(<Card key={league.id} color='blue'><League endDate={league.end_date}balance={league.start_balance} name={league.name} id={league.id}/></Card>))}</Card.Group>:<h1>Go Join a League!</h1>}
       </div>
     </div>
   )
